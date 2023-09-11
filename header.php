092a0353795063ed3ae6ea3d31ca11fbca9eeca9
David <134588042+Akadid@users.noreply.github.com>
2023-09-11 16:05:27 +0200

    CLEAN version index base

--- header.php
+++ header.php
@@ -1,22 +1,5 @@
-<?php
-require_once('config/autoload.php');
-require_once('config/db.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CompareOperators</title>
-    <link rel="stylesheet" href="css/style.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    
-</head>
-
-<body id="page-top">
-    <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
+<header id="page-top">
+    <nav class="navbar navbar-light bg-warning navbar-expand-lg fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#page-top"><img src="images/logo-yellow.png" alt="CompareOperators" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ms-1"></i>
@@ -31,6 +14,4 @@ require_once('config/db.php');
             </div>
         </div>
     </nav>
-</body>
-
-</html>
+</header>
