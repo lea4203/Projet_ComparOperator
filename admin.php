92524744d5e383cc6cffea42ac60379acddc5f0a
lea4203 <lea.merley@orange.fr>
2023-09-12 14:12:23 +0200

    ajout de la page admin et des informations dans cette page

---
+++ admin.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once 'classes/Manager.php';
+require_once 'config/db.php';
+$manager = new Manager($db);
+$destinations = $manager->getAllDestination();
+
+$tourOperators = $manager->getAllTourOperator();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Liste des Destinations</title>
+</head>
+
+<body class="admin_header">
+
+    <div class="container mt-5">
+        <h1>Liste des Destinations</h1>
+
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Location</th>
+                    <th>Prix</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($destinations as $destination): ?>
+                    <tr>
+                        <td>
+                            <?php echo $destination['location']; ?>
+                        </td>
+                        <td>
+                            <?php echo $destination['price']; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <a href="form_ajout_destination.php" class="btn btn-primary" style="color:  #051c55">Ajouter une destination</a>
+    </div>
+
+    <div class="container mt-5">
+        <h1>Liste des Tour Operators</h1>
+
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Nom du tour operator</th>
+                    <th>Grade</th>
+                    <th>Grade total</th>
+                    <th>Lien</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($tourOperators as $tourOperator): ?>
+                    <tr>
+                        <td>
+                            <?php echo $tourOperator['name']; ?>
+                        </td>
+                        <td>
+                            <?php echo $tourOperator['grade_count']; ?>
+                        </td>
+                        <td>
+                            <?php echo $tourOperator['grade_total']; ?>
+                        </td>
+                        <td>
+                            <?php echo $tourOperator['link']; ?>
+                        </td>
+                        <td>
+                            <form action="modifier_tour_operator.php" method="post" style="display: inline;">
+                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                                <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
+                            </form>
+                            <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
+                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                                <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+
+    <a href="form_ajout_tour_operator.php" class="btn btn-primary" style="color:  #051c55">Ajouter une Tour
+        Opérateur</a>
+    </div>
+
+</body>
+
+</html>
---
+++ form_ajout_tour_operator.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Tour Operator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Formulaire de Tour Operator</h1>
+        <form action="traitement/traitement_form_tour_operator.php" method="post">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom du tour operator</label>
+                <input type="text" class="form-control" id="name" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="grade_count" class="form-label">Note global</label>
+                <input type="text" class="form-control" id="grade_count" name="grade_count">
+            </div>
+            <div class="mb-3">
+                <label for="grade_total" class="form-label">Note Total</label>
+                <input type="text" class="form-control" id="grade_total" name="grade_total">
+            <div class="mb-3">
+                <label for="link" class="form-label">Lien</label>
+                <input type="text" class="form-control" id="link" name="link">
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
