dd907afed963628ecf9d3b64b5d8f6fb03310447
David <134588042+Akadid@users.noreply.github.com>
2023-09-14 09:53:16 +0200

    Style Admin

--- admin/index.php
+++ admin/index.php
@@ -15,8 +15,10 @@ $tourOperators = $manager->getAllTourOperator();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
     <title>Liste des Destinations</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
 <header>
@@ -24,9 +26,10 @@ $tourOperators = $manager->getAllTourOperator();
 </header>
 
 <body class="admin_header">
-
-    <div class="container mt-5">
-        <h1>Liste des Destinations</h1>
+        <div class="pt-5 pb-2 m-3 bg-secondary text-white text-center opacity-50">
+            <h2>Gestion du site</h2></div>
+        <div class="container col-8 align-item-center pb-3 border-bottom border-secondary">
+        <h3>Liste des Destinations</h3>
 
         <table class="table table-bordered">
             <thead class="thead-dark">
@@ -49,57 +52,53 @@ $tourOperators = $manager->getAllTourOperator();
             </tbody>
         </table>
 
-        <a href="form_ajout_destination.php" class="btn btn-primary" style="color:  #051c55">Ajouter une destination</a>
+        <a href="form_ajout_destination.php" class="btn btn-primary text-white" style="color:  #051c55">Ajouter une destination</a>
     </div>
 
-    <div class="container mt-5">
-        <h1>Liste des Tour Operators</h1>
+    <div class="container col-8 align-item-center pt-3 pb-3 mb-5border-bottom border-secondary">
+    <h3>Liste des Tour Operators</h3>
 
-        <table class="table table-bordered">
-            <thead class="thead-dark">
+    <table class="table table-bordered">
+        <thead class="thead-dark">
+            <tr>
+                <th>Nom du tour operator</th>
+                <th>Grade</th>
+                <th>Grade total</th>
+                <th>Lien</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($tourOperators as $tourOperator): ?>
                 <tr>
-                    <th>Nom du tour operator</th>
-                    <th>Grade</th>
-                    <th>Grade total</th>
-                    <th>Lien</th>
-                    <th>Actions</th>
+                    <td>
+                        <?php echo $tourOperator['name']; ?>
+                    </td>
+                    <td>
+                        <?php echo $tourOperator['grade_count']; ?>
+                    </td>
+                    <td>
+                        <?php echo $tourOperator['grade_total']; ?>
+                    </td>
+                    <td>
+                        <?php echo $tourOperator['link']; ?>
+                    </td>
+                    <td>
+                        <form action="modifier_tour_operator.php" method="post" style="display: inline;">
+                            <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                            <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
+                        </form>
+                        <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
+                            <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                            <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($tourOperators as $tourOperator): ?>
-                    <tr>
-                        <td>
-                            <?php echo $tourOperator['name']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['grade_count']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['grade_total']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['link']; ?>
-                        </td>
-                        <td>
-                            <form action="modifier_tour_operator.php" method="post" style="display: inline;">
-                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                                <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
-                            </form>
-                            <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
-                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                                <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-
-
-    <a href="form_ajout_tour_operator.php" class="btn btn-primary" style="color:  #051c55">Ajouter une Tour
-        Opérateur</a>
-    </div>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <a href="form_ajout_tour_operator.php" class="btn btn-primary text-white" style="color: #051c55">Ajouter une Tour Opérateur</a>
+</div>
 
 </body>
 
