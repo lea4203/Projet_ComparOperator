201b49d1492d9d2efffa52002decc4aedbfa320e
David <134588042+Akadid@users.noreply.github.com>
2023-09-14 10:00:46 +0200

    Maj Style admin

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,7 @@ $tourOperators = $manager->getAllTourOperator();
 </header>
 
 <body class="admin_header">
-        <div class="pt-5 pb-2 m-3 bg-secondary text-white text-center opacity-50">
+        <div class="pt-4 pb-1 m-3 bg-secondary text-white text-center opacity-50">
             <h2>Gestion du site</h2></div>
         <div class="container col-8 align-item-center pb-3 border-bottom border-secondary">
         <h3>Liste des Destinations</h3>
@@ -99,14 +99,14 @@ $tourOperators = $manager->getAllTourOperator();
                         <?php echo $tourOperator['link']; ?>
                     </td>
                     <td>
-                        <form action="modifier_tour_operator.php" method="post" style="display: inline;">
-                            <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                            <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
-                        </form>
-                        <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
-                            <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                            <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
-                        </form>
+                    <form action="modifier_tour_operator.php" method="get" style="display: inline;">
+                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                                <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
+                            </form>
+                            <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
+                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
+                                <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
+                            </form>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -115,48 +115,6 @@ $tourOperators = $manager->getAllTourOperator();
     <a href="form_ajout_tour_operator.php" class="btn btn-primary text-white" style="color: #051c55">Ajouter une Tour Opérateur</a>
 </div>
 
-        <table class="table table-bordered">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Nom du tour operator</th>
-                    <th>Grade</th>
-                    <th>Grade total</th>
-                    <th>Lien</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($tourOperators as $tourOperator): ?>
-                    <tr>
-                        <td>
-                            <?php echo $tourOperator['name']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['grade_count']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['grade_total']; ?>
-                        </td>
-                        <td>
-                            <?php echo $tourOperator['link']; ?>
-                        </td>
-                        <td>
-                            <form action="modifier_tour_operator.php" method="get" style="display: inline;">
-                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                                <button type="submit" class="btn custom-primary-btn btn-sm">Modifier</button>
-                            </form>
-                            <form action="supprimer_tour_operator.php" method="post" style="display: inline;">
-                                <input type="hidden" name="id" value="<?php echo $tourOperator['id']; ?>">
-                                <button type="submit" class="btn custom-danger-btn btn-sm">Supprimer</button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-
-    <a href="form_ajout_tour_operator.php" class="btn btn-primary" style="color: #051c55">Ajouter une Tour Opérateur</a>
 </body>
 
 </html>
